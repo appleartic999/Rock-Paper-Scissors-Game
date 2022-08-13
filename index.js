879572f900b4810e962b7eb90399ed071892fc07
Apple Artic <techapple652@gmail.com>
2022-08-13 17:35:21 +0530

    added number of games player wins at end of all games

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 const choices = ["rock", "paper", "scissors"];
 
-const game = function (playerSelection,computerSelection) {
+const game = function (playerSelection, computerSelection) {
     if (playerSelection === "rock") {
-        if (computerSelection === "scissors") console.log(`You Win! ${playerSelection.toLocaleUpperCase()} beats ${computerSelection.toLocaleUpperCase()}`);
-        else if (computerSelection === "paper") console.log(`You Lose! ${computerSelection.toLocaleUpperCase()} beats ${playerSelection.toLocaleUpperCase()}`);
-        else console.log(`It's a Draw! Both chose ${playerSelection.toLocaleUpperCase()}`);
+        if (computerSelection === "scissors") {
+            console.log(`You Win! ${playerSelection.toLocaleUpperCase()} beats ${computerSelection.toLocaleUpperCase()}`);
+            return 1;
+        }
+        else if (computerSelection === "paper") {
+            console.log(`You Lose! ${computerSelection.toLocaleUpperCase()} beats ${playerSelection.toLocaleUpperCase()}`);
+            return 0;
+        }
+        else {
+            console.log(`It's a Draw! Both chose ${playerSelection.toLocaleUpperCase()}`);
+            return 2;
+        }
     } else if (playerSelection === "paper") {
-        if (computerSelection === "rock") console.log(`You Win! ${playerSelection.toLocaleUpperCase()} beats ${computerSelection.toLocaleUpperCase()}`);
-        else if (computerSelection === "scissors") console.log(`You Lose! ${computerSelection.toLocaleUpperCase()} beats ${playerSelection.toLocaleUpperCase()}`);
-        else console.log(`It's a Draw! Both chose ${playerSelection.toLocaleUpperCase()}`);
+        if (computerSelection === "rock") {
+            console.log(`You Win! ${playerSelection.toLocaleUpperCase()} beats ${computerSelection.toLocaleUpperCase()}`);
+            return 1;
+        }
+        else if (computerSelection === "scissors") {
+            console.log(`You Lose! ${computerSelection.toLocaleUpperCase()} beats ${playerSelection.toLocaleUpperCase()}`);
+            return 0;
+        }
+        else {
+            console.log(`It's a Draw! Both chose ${playerSelection.toLocaleUpperCase()}`);
+            return 2;
+        }
     } else if (playerSelection === "scissors") {
-        if (computerSelection === "paper") console.log(`You Win! ${playerSelection.toLocaleUpperCase()} beats ${computerSelection.toLocaleUpperCase()}`);
-        else if (computerSelection === "rock") console.log(`You Lose! ${computerSelection.toLocaleUpperCase()} beats ${playerSelection.toLocaleUpperCase()}`);
-        else console.log(`It's a Draw! Both chose ${playerSelection.toLocaleUpperCase()}`);
+        if (computerSelection === "paper") {
+            console.log(`You Win! ${playerSelection.toLocaleUpperCase()} beats ${computerSelection.toLocaleUpperCase()}`);
+            return 1;
+        }
+        else if (computerSelection === "rock") {
+            console.log(`You Lose! ${computerSelection.toLocaleUpperCase()} beats ${playerSelection.toLocaleUpperCase()}`);
+            return 0;
+        }
+        else {
+            console.log(`It's a Draw! Both chose ${playerSelection.toLocaleUpperCase()}`);
+            return 2;
+        }
     } else {
         console.log("Invalid choice");
         alert("Enter a valid name as shown");
@@ -20,10 +47,19 @@ const game = function (playerSelection,computerSelection) {
     }
 }
 
-for (let i = 0; i < 5; i++){
-    const randomChoice = Math.floor(Math.random() * 3);
-    const computerSelection = choices[randomChoice];
-    const playerSelection = prompt("Type your choice").toString().toLowerCase();
-    const valid = game(playerSelection, computerSelection);
-    if (valid===false) i--;
+
+const playGround = function () {
+    const countOfPlayerWins = 0;
+    for (let i = 0; i < 5; i++) {
+        const randomChoice = Math.floor(Math.random() * 3);
+        const computerSelection = choices[randomChoice];
+        const playerSelection = prompt("Type your choice").toString().toLowerCase();
+        const valid = game(playerSelection, computerSelection);
+        if (valid === false) i--;
+        if (valid === 1) countOfPlayerWins++;
+        else continue;
+    }
+    return countOfPlayerWins;
 }
+
+alert(`You wins ${playGround()} matches out of 5`);
